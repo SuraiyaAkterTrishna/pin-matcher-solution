58ddd4ba92c341452900a8ad8be5b84e18762ee6
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-08 15:15:22 +0600

    generate random 4 digit pin done

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-function getPin(){
-    const pin = generatePin();
-    const pinString = pin + '';
-    if(pinString.length === 4){
-        return pin;
-    }
-    else{
-        console.log('4 digit not found', pin);
-        return getPin();
-    }
-}
 function generatePin(){
     const random = Math.round(Math.random()*10000);
     return random;
