979c14aba95f469d3b754e8991704fa7a6aae4e4
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-08 15:23:16 +0600

    onclick button generate pin

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ function getPin(){
         return pin;
     }
     else{
+        console.log('3 digit pin found');
         return getPin();
     }
 }
@@ -12,3 +13,8 @@ function generatePin(){
     const random = Math.round(Math.random()*10000);
     return random;
 }
+// generate 4 digit pin 
+document.getElementById('generate-pin').addEventListener('click', function(){
+    const pin = getPin();
+    console.log(pin);
+})
