c91038a6252ef6577b587620fc1223b2c4ce6220
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-08 15:09:00 +0600

    pin generate done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('app.js connected');
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+        console.log('4 digit not found', pin);
+        return getPin();
+    }
+}
+function generatePin(){
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
